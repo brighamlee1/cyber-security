f1911146d5e1407c78ab849329836f04ea86699a
brighambroadbent <brighambroadbent@gmail.com>
2023-12-05 23:37:58 -0600

    blah

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-export default {
-    server: {
-        // specify the allowed paths
-        // add the path in question to the allow list
-        allowList: ['/Users/brighambroadbent/node_modules/ofetch/dist/index.mjs'],
-    },
-};
